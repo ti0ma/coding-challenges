e5d3beb8e56928b16bad0b9e0f9aa401fe2a8a7e
Artem Molonosov <artmol91@gmail.com>
2018-11-24 14:31:36 +0100

    Solved the challenge

--- adidas-ascii-logo/typescript/src/adidas-ascii-logo.ts
+++ adidas-ascii-logo/typescript/src/adidas-ascii-logo.ts
@@ -4,5 +4,29 @@
  * @returns adidas logo.
  */
 export function createAdidasAsciiLogo(width: number): string {
-  return `${ width }`;
+  if (width < 2) {
+    throw new Error('Error, minimun width is 2');
+  }
+
+  const height: number = Math.round(Math.sqrt(width));
+  const stripe = '@'.repeat(width);
+  const space = ' '.repeat(height);
+  // The 'invisible' stripe
+  const whiteSpace = ' '.repeat(width);
+
+  const logo: Array<string> = [];
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < height; j++) {
+      const stripes = `${space}${stripe}`.repeat(3 - i);
+      const prevSpace = `${whiteSpace}`.repeat(i);
+
+      const row = `${prevSpace}${stripes}`;
+
+      // Skipping the first character because of an extra space added.
+      logo.push(row.substring(j + 1));
+    }
+  }
+
+  return logo.reverse().join('\n');
 }
